b6c7b94d63131be75754d4bd60690c7927520f90
yul402 <Ying.li20@outlook.com>
2023-06-05 10:46:37 -0500

    Merge branch 'main' of https://github.com/Elias-Montes/MovieVault-Project

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,19 @@
 var userSearch
 var apiKey = "9920fb12bf2316b80811a61d121e9ee8"
 var trendingTitle = $(".trending")
-//var trendingDiv = $(".trending-div")
 var trendingImage = $(".trending-image")
 var modalUrl = ""
 
-//null poster path: pexel
-//0Hruyh1T07GCKyGt53A7CckpNVq9P0vPx4d87asBLvmQ44T79APdbS8L
-function picture(){
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: '0Hruyh1T07GCKyGt53A7CckpNVq9P0vPx4d87asBLvmQ44T79APdbS8L'
-    }
-  };
-  
-  fetch("https://api.pexels.com/v1/curated?per_page=4", options)
-  .then(response => response.json())
-  .then(function(data){
-    console.log(data)})
-  }
 
 //Set up trailer
 $(".trending-image").on("click",function(event){
-  //$(event.target).attr("data-trend-trailer"))
   var modalUrl = "https://www.youtube.com/embed/"+$(event.target).attr("data-trend-trailer")+"?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1"
   $("iframe").attr("src",modalUrl)
-  //console.log(modalUrl)
 })
 
 //Reload modal
 $("#close-modal").on("click",function(){
   $("iframe").attr("src",modalUrl)
-  //console.log(modalUrl)
 })
 
 // Redirect to Search.html with user search input
@@ -44,13 +24,6 @@ $("#search-button").on("click",function(){
   }else{
       window.location.href="./search.HTML?search="+userSearch;
   }
-})
-
-// Store to localStorage
-$("#add-list").on("click",function(){
-  console.log("Been add to ist")
-  console.log($(this))
-
 })
 
 // Trending List
@@ -66,26 +39,22 @@ function trendingLoad(){
     fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
       .then(response => response.json())
       .then(function(data){
-        //console.log($(".trending-image"))
         console.log(data)
-        for (i=0; i<4; i++){
+        for (i=0; i<8; i++){
           trendingTitle[i].textContent=data.results[i].title
-          //$(trendingDiv[i]).attr("data-id",data.results[i].id)//Store trending film id 
           getPoster(data.results[i].poster_path,$(trendingImage[i]))
           gettrailerKey(data.results[i].id,$(trendingImage[i]))
         }
       })
 }
 
-            //getTrailer(data.results[i].poster_path,$(trendingImage[i]))
-
+// Load trending poster
 function getPoster(posterPath,Ele){
   var trendingPoster = "https://image.tmdb.org/t/p/w500" + posterPath
   Ele.attr("src",trendingPoster)
 }
 
-
-// Get trailer key
+// Get trending movie's trailer key
 function gettrailerKey(filmID,Ele){
   var urlTrailer = "https://api.themoviedb.org/3/movie/" + filmID + "/videos?api_key=9920fb12bf2316b80811a61d121e9ee8'"
 
@@ -100,30 +69,35 @@ function gettrailerKey(filmID,Ele){
   fetch(urlTrailer, options)
     .then(response => response.json())
     .then(function(data){
-      //Get "official Trailer" or "Trailer"
-      //console.log(data)
       var trailer = data.results.filter(function(el){
         return el.name === "Official Trailer"})
 
       if (!trailer[0]){var trailer = data.results.filter(function(el){
         return el.type = "Trailer"})}              
-      //return trailer[0]
-      //console.log(trailer[0])
-
-      // console.log('been here')
-      // console.log(trailer[0].key)
       Ele.attr("data-trend-trailer",trailer[0].key)
       Ele.attr("data-id",filmID)
-
-      //return trailer[0].key
-
-      // $('iframe').attr("src","https://www.youtube.com/embed/"+trailer[0].key+"modestbranding=1&rel=0&controls=0&showinfo=0&html5=1")
-      // <iframe width="560" height="315" src="https://www.youtube.com/embed/SUXWAEX2jlg?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" style="margin:0 auto" allowfullscreen></iframe>
-
-      //Get key
-      //Update ifram link
     })
 
+}
+
+//function to load the list page on click at homepage
+function listLoad(tabName) {
+  var url = "/list.HTML";
+  var targetTab;
+  console.log(tabName);
+  switch (tabName) {
+      case "tabMovies":
+          targetTab = "moviesTabBtn";
+          break;
+      case "tabTvshows":
+          targetTab = "tvShowsTabBtn";
+          break;
+      default:
+          targetTab = "moviesTabBtn";
+          break;
+  }
+  
+  window.open(url, targetTab);
 }
 
 trendingLoad()
